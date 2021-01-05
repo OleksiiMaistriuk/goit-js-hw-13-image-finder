07832ee5503ccc11634af8073ec04c189ddd641e
OleksiiMaistriuk <65893898+OleksiiMaistriuk@users.noreply.github.com>
2021-01-05 23:49:57 +0100

    сделал шаблон

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles.css';
+import '../node_modules/material-design-icons/iconfont/material-icons.css';
+import count from './count.json';
+// import galleryTemplates from '../templates/gallery.hbs';
+import photoCardTemplates from '../templates/photo-card.hbs';
+// import searchFormTemplates from '../templates/search-form.hbs';
+
+const refs = {
+  photoCardConteiner: document.querySelector('.photo-card'),
+};
+
+// const galery = galleryTemplates();
+const photoCard = photoCardTemplates(count);
+// const searchForm = searchFormTemplates();
+
+refs.photoCardConteiner.insertAdjacentHTML('beforeend', photoCard);
