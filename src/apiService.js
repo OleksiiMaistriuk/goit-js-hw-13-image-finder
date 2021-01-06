70e463f4edd0967462d3be6fee99dafde15c142e
OleksiiMaistriuk <65893898+OleksiiMaistriuk@users.noreply.github.com>
2021-01-07 00:23:33 +0100

    create load button

--- src/apiService.js
+++ src/apiService.js
@@ -1,7 +1,25 @@
-export default function fetchImages(searchQuery, pageNumber) {
-  return fetch(
-    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key=16958018-5870f5be4b9a0ed4e53bb3835`,
-  )
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
+export default {
+  searchQuery: '',
+  pageNumber: 1,
+
+  fetchImages() {
+    return fetch(
+      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.pageNumber}&per_page=12&key=16958018-5870f5be4b9a0ed4e53bb3835`,
+    )
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.pageNumber += 1;
+        return hits;
+      })
+      .catch(error => console.log(error));
+  },
+  resetPage() {
+    this.pageNumber = 1;
+  },
+  get query() {
+    return this.searchQuery;
+  },
+  set query(value) {
+    this.searchQuery = value;
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,38 +1,32 @@
 import './styles.css';
 import '../node_modules/material-design-icons/iconfont/material-icons.css';
-import count from './count.json';
-// import galleryTemplates from '../templates/gallery.hbs';
+import apiService from './apiService';
 import photoCardTemplates from '../templates/photo-card.hbs';
+
+// import galleryTemplates from '../templates/gallery.hbs';
 // import searchFormTemplates from '../templates/search-form.hbs';
 
 const refs = {
   photoCardConteiner: document.querySelector('.photo-card'),
   searchForm: document.querySelector('#search-form'),
+  loadMoreButton: document.querySelector('.js-button'),
 };
 
-console.log(refs.searchForm);
 refs.searchForm.addEventListener('submit', event => {
   event.preventDefault();
-  console.log();
+  const form = event.currentTarget;
+  apiService.query = form.elements.query.value;
+  refs.photoCardConteiner.innerHTML = '';
+  form.reset();
+  apiService.resetPage();
+  apiService.fetchImages().then(apdateArticlesMurkup);
 });
 
-// const debounceInput = event => {
-//   fetchImages(event.target.value).then(updateArticlesMurkup);
-// };
-
-// const photoCard = photoCardTemplates(fetchImages());
-
-function fetchImages(searchQuery, pageNumber) {
-  return fetch(
-    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key=16958018-5870f5be4b9a0ed4e53bb3835`,
-  )
-    .then(response => response.json())
-    .then(({ hits }) => {
-      console.log(hits);
-      const markup = photoCardTemplates(hits);
-      console.log(markup);
-      refs.photoCardConteiner.insertAdjacentHTML('beforeend', markup);
-    })
-    .catch(error => console.log(error));
+function apdateArticlesMurkup(hits) {
+  const markup = photoCardTemplates(hits);
+  refs.photoCardConteiner.insertAdjacentHTML('beforeend', markup);
 }
-fetchImages('poland', 2);
+
+refs.loadMoreButton.addEventListener('click', () => {
+  apiService.fetchImages().then(apdateArticlesMurkup);
+});
